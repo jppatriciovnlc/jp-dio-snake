a0164593b29e72ccf61fe40381a61a7eae755a4d
jppatriciovnlc <joao.patricio@vanillacorp.com.br>
2021-06-23 20:43:52 -0300

    incluir contador

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let score = document.getElementById("score");
+
+let box = 32;       // 
 let snake = [];
+let counter = 0;
+let direction = "right";
+
 snake[0]= {
     x: 8 * box,
     y: 8 * box
-}
-let direction = "right";
+};
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
-}
+};
 
+score.innerText = counter;
+document.addEventListener('keydown', update);
+
+
+
+
+
+// default size: 512px x 512px  (16*32)
 function createBackG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box);
@@ -24,23 +36,24 @@ function createSnake(){
     }
 }
 
-function drawFood(){
+function createFood(){
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box)
 }
 
-document.addEventListener('keydown', update);
+
 
 function update(event){
     console.log(event.keyCode)
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down";
-    
+    if(event.keyCode == 40 && direction != "up") direction = "down";    
 }
 
 function initGame(){
+
+    
 
     console.log("direction=" + direction)
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
@@ -57,7 +70,7 @@ function initGame(){
 
     createBackG();
     createSnake();
-    drawFood();
+    createFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -71,8 +84,10 @@ function initGame(){
         snake.pop();
     }
     else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box,
-        food.y = Math.floor(Math.random() * 15 + 1) * box
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+        counter++;
+        score.innerText = counter;
     }
 
 
